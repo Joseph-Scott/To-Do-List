6cfa70bd0083b651afdabcefa3843794666ca7ed
Joseph Scott <jbs198585@gmail.com>
2019-08-07 05:37:10 -0400

    toggle gray and line through

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,7 +1,20 @@
 // Check off specific to do's by clicking
 $("li").click(function(){
-	$(this).css({
-		color: "gray",
-		textDecoration: "line-through"
-	});
+	//if li is gray
+	if($(this).css("color") === "rgb(128, 128, 128)"){
+		//turn it black
+		$(this).css({
+			color: "black", 
+			textDecoration: "none"
+		});
+	}
+	//else
+	else {
+		//turn it black
+		$(this).css({
+			color: "gray",
+			textDecoration: "line-through"
+		});
+	}
+
 });
