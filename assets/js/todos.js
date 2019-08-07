089ae51bf05fcb742a15dc93c881dfb9638a63ad
Joseph Scott <jbs198585@gmail.com>
2019-08-07 06:48:07 -0400

    input functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,22 @@
 // Check off specific to do's by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 // Click on X to delete To Do
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
-})
+});
+
+$("input[type='text'").keypress(function(event){
+	if(event.which === 13){
+		// Grabbing new to do text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		// Create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
+});
