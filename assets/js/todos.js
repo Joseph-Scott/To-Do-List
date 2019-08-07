f43f8713dfa1bd875eb63ec59264f3dd5a02a7e3
Joseph Scott <jbs198585@gmail.com>
2019-08-07 05:38:36 -0400

    refactor with css

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,20 +1,4 @@
 // Check off specific to do's by clicking
 $("li").click(function(){
-	//if li is gray
-	if($(this).css("color") === "rgb(128, 128, 128)"){
-		//turn it black
-		$(this).css({
-			color: "black", 
-			textDecoration: "none"
-		});
-	}
-	//else
-	else {
-		//turn it black
-		$(this).css({
-			color: "gray",
-			textDecoration: "line-through"
-		});
-	}
-
+	$(this).toggleClass("completed");
 });
